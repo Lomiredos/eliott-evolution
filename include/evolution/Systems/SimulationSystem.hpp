a62f1f357eb886a7b3bc4bd926d45aaf291179c3
Lomiredos <ebaudoin@gaming.tech>
2026-04-08 00:48:41 +0200

    Add the Function createRandom on the simulationSystem

---
+++ include/evolution/Systems/SimulationSystem.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "evolution/Components/NeuralNetwork.hpp"
+
+
+#include "ecs/System.hpp"
+#include "ecs/World.hpp"
+
+#include <random>
+
+
+class SimulationSystem : public ee::ecs::System {
+
+private:
+    std::mt19937 m_rng;
+    std::uniform_real_distribution<float> m_dist;
+
+
+public:
+    SimulationSystem(): m_rng(std::random_device{}()), m_dist(-1.0f, 1.0f) {}
+
+    void update(ee::ecs::World& _world, float _dt) override {
+
+    }
+
+private:
+
+
+    NeuralNetwork createRandom(const std::vector<int>& _topology) {
+        NeuralNetwork nn;
+
+        for (size_t i = 0; i < _topology.size() - 1; i++){
+            
+            int nbEntrees = _topology[i];
+            int nbNeurones = _topology[i +1];
+
+            std::vector<float> layerWeights;
+            for (size_t j = 0; j < nbEntrees * nbNeurones; j++) layerWeights.push_back(m_dist(m_rng));
+            nn.weights.push_back(layerWeights);
+            
+            std::vector<float> layerBiases;
+            for (size_t j = 0; j < nbNeurones; j++) layerBiases.push_back(m_dist(m_rng));
+            nn.biases.push_back(layerBiases);
+        }
+        nn.fitness = 0.f;
+        return nn;
+    }
+
+
+};
