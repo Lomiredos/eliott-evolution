0375bfe8f2085d51cc6272220e236505e25596c6
Lomiredos <ebaudoin@gaming.tech>
2026-04-08 12:36:19 +0200

    Add evolution - selection, mutation, generational loop

--- include/evolution/Systems/SimulationSystem.hpp
+++ include/evolution/Systems/SimulationSystem.hpp
@@ -11,6 +11,8 @@
 #include <random>
 
 
+#include <algorithm>
+
 class SimulationSystem : public ee::ecs::System {
 
 private:
@@ -19,6 +21,10 @@ private:
     std::mt19937 m_rng;
     std::uniform_real_distribution<float> m_dist;
 
+
+    float m_generationTime = 30.f;  
+    float m_timer = 0.f;            
+    int m_generation = 0;  
 
 public:
     SimulationSystem(): m_rng(std::random_device{}()), m_dist(-1.0f, 1.0f) {}
@@ -30,6 +36,16 @@ public:
     }
 
     void update(ee::ecs::World& _world, float _dt) override {
+
+        m_timer += _dt;
+        if (m_timer > m_generationTime){
+            m_generation++;
+            evolve(_world);
+            m_timer = 0.f;
+            return;
+        }
+
+
         for (ee::ecs::EntityID id: m_entities){
             sf::Vector2f creaturePos = _world.getComponent<Transform>(id).position;
             NeuralNetwork& neural = _world.getComponent<NeuralNetwork>(id);
@@ -38,10 +54,7 @@ public:
                 sf::Vector2f foodPos = _world.getComponent<Transform>(foodId).position;
         
                 if (getDistance(creaturePos, foodPos) > 15) continue;
-                
-std::cout << "Food mangee ! Distance: " 
-          << getDistance(creaturePos, foodPos) << std::endl;
-
+            
 
                 neural.fitness++;
                 _world.markForDestruction(foodId);
@@ -71,9 +84,84 @@ std::cout << "Food mangee ! Distance: "
         return nn;
     }
 
-    private:
+private:
     float getDistance(const sf::Vector2f& a, const sf::Vector2f& b) const {
         return sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
+    }
+
+    void evolve(ee::ecs::World& _world){
+    std::uniform_real_distribution<float> distX(50.f, 1230.f);
+    std::uniform_real_distribution<float> distY(50.f, 670.f);
+    std::uniform_real_distribution<float> distDir(0.f, 360.f);
+
+
+
+    std::vector<ee::ecs::EntityID> creatureID;
+    for (ee::ecs::EntityID id : m_entities) creatureID.push_back(id);
+
+    std::sort(creatureID.begin(), creatureID.end(), [&_world](const auto& a, const auto& b)
+    {
+        return _world.getComponent<NeuralNetwork>(a).fitness > _world.getComponent<NeuralNetwork>(b).fitness;  
+    });
+
+
+    int nbParents = std::max(1, (int)(creatureID.size() * 0.1f));
+    creatureID.erase(creatureID.begin() + nbParents, creatureID.end());
+
+    std::vector<NeuralNetwork> parentsBrains;
+    for (ee::ecs::EntityID id : creatureID) parentsBrains.push_back(_world.getComponent<NeuralNetwork>(id));
+    
+    creatureID.clear();
+
+    for (ee::ecs::EntityID id : m_entities){
+        _world.markForDestruction(id);
+    }
+
+    std::uniform_int_distribution<int> distP(0, nbParents - 1);
+
+    for (int i = 0; i < nbParents * 10; i++){
+
+        NeuralNetwork child = mutate(parentsBrains[distP(m_rng)]);
+
+        ee::ecs::EntityID creature = _world.createEntity();
+        _world.addComponent(creature, Transform{ sf::Vector2f(distX(m_rng), distY(m_rng)) });
+        _world.addComponent(creature, Motion{ distDir(m_rng), 90.f, 0.f, 150.f });
+        _world.addComponent(creature, Vitals{ 100.f, 100.f, 0.f });
+        std::vector<Ray> rays;
+        for (int i = -2; i <= 2; i++){
+            rays.push_back(Ray{100.f, (float)(i * 30), 100.f, DetectedType::Nothing});
+        }
+        _world.addComponent(creature, Vision{rays});
+        _world.addComponent(creature, child);
+    }
+
+    for (int i = 0; i < 10; i++){
+        ee::ecs::EntityID food = _world.createEntity();
+        _world.addComponent(food, Transform{ sf::Vector2f(distX(m_rng), distY(m_rng)) });
+        _world.addComponent(food, Food{});
+    }
+
+
+    }
+
+    NeuralNetwork mutate(const NeuralNetwork& _parent)
+    {
+        NeuralNetwork child = _parent;  
+        
+        std::uniform_real_distribution<float> mutation(-0.1f, 0.1f);
+        
+        for (int i = 0; i< child.weights.size(); i++){
+            for (int j = 0; j < child.weights[i].size(); j++){
+                child.weights[i][j] += mutation(m_rng);
+            }
+        }
+        for (int i = 0; i< child.biases.size(); i++){
+            for (int j = 0; j < child.biases[i].size(); j++){
+                child.biases[i][j] += mutation(m_rng);
+            }
+        }
+        child.fitness = 0.f; 
+        return child;
     }
 
 };
